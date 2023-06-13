7bd8c2d1377f9ccdf08aa12b1a0fbf5a2c21e396
Maxime <maxime.le-besnerais@epitech.eu>
2023-06-13 22:50:24 +0200

    [ALL] 7 files were committed 	Files: 		.idea/: Untracked 		.vscode/: Untracked 		Program.cs: Untracked 		TodoMaker.csproj: Untracked 		bin/: Untracked 		obj/: Untracked 		todos.json: Untracked
    	Comment:
    		Committed all files

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using Nancy;
+using System.Linq;
+using System;
+using Nancy.Hosting.Self;
+
+public class Activity
+{
+    public string title { get; set; }
+    public string description { get; set; }
+    public string dueDate { get; set; }
+    public bool done { get; set; }
+}
+
+public class ActivitiesModule : NancyModule
+{
+    private List<Activity> _activities;
+
+    public ActivitiesModule() : base("/API")
+    {
+        _activities = LoadActivitiesFromFile();
+
+        Get("/", _ => "no data");
+
+        Get("/all", _ => Response.AsJson(_activities.Select(a => a.title)));
+
+        Get("/t_search/{title}", parameters =>
+        {
+            var activities = _activities.Where(a => a.title.Contains((string)parameters.title, StringComparison.OrdinalIgnoreCase));
+            return Response.AsJson(activities.Select(a => a.title));
+        });
+
+        Get("/d_search/{description}", parameters =>
+        {
+            var activities = _activities.Where(a => a.description.Contains((string)parameters.description, StringComparison.OrdinalIgnoreCase));
+            return Response.AsJson(activities.Select(a => a.title));
+        });
+
+        Get("/see/{title}", parameters =>
+        {
+            var title = ((string)parameters.title).Replace('_', ' ');
+            var activity = _activities.FirstOrDefault(a => a.title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            return activity != null
+                ? Response.AsJson(activity)
+                : HttpStatusCode.NotFound;
+        });
+
+    }
+
+    private List<Activity> LoadActivitiesFromFile()
+    {
+        var jsonString = File.ReadAllText("todos.json");
+        var activities = JsonSerializer.Deserialize<List<Activity>>(jsonString);
+        return activities;
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var uri = new Uri("http://localhost:1234");
+        var hostConfiguration = new HostConfiguration
+        {
+            UrlReservations = new UrlReservations() { CreateAutomatically = true }
+        };
+
+        // Initialize an instance of NancyHost
+        using (var host = new NancyHost(hostConfiguration, uri))
+        {
+            host.Start(); // start hosting
+
+            // Under here, you could add any logic you want to happen after the server starts
+            Console.WriteLine("Your application is running on " + uri);
+            Console.WriteLine("Press any [Enter] to close the Nancy host.");
+            Console.ReadLine();
+        }
+    }
+}
